47b404332a13ba8d3e151d7c2e5fac7ca83dc9b4
Willian Rupert <willianrupert@gmail.com>
2025-10-09 14:27:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install -r <(pipenv lock -r)
+# Instala as dependências do Pipfile diretamente no ambiente do sistema
+pipenv install --system --deploy
 
+# Executa os comandos de build do Django
 python manage.py collectstatic --no-input
 python manage.py migrate
