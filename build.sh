d929703cb71f1be9c824dcc888bf3bb4253c6862
Willian Rupert <willianrupert@gmail.com>
2025-10-13 12:29:27 -0300

    Update: Popula as perguntas do questionário

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ pipenv install --system --deploy
 # Executa os comandos de build do Django
 python manage.py collectstatic --no-input
 python manage.py migrate
+# Popula as perguntas do questionário
+python manage.py populate_questions
 
 # Cria o superusuário se as variáveis de ambiente estiverem definidas
 if [[ -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_PASSWORD" && -n "$DJANGO_SUPERUSER_EMAIL" ]]; then
